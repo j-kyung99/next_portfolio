d89e0f4459c09c34792027ab06609c7c8c6448f1
j-kyung99 <apfwnls14@naver.com>
2023-03-14 16:34:13 +0900

    Update: Header, Footer

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>빡코딩 포트폴리오</title>
         <meta name="description" content="오늘도 빡코딩!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>오늘도 빡코딩!</h1>
-    </div>
+      <h1>홈 입니다</h1>
+    </Layout>
   );
 }
