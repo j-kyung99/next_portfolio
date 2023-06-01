511c73f167d6d2c07d63b5d72a0f2b563af62f5d
j-kyung99 <apfwnls14@naver.com>
2023-06-01 13:38:02 +0900

    Add: Creating a project interface

--- pages/projects.js
+++ pages/projects.js
@@ -1,21 +1,29 @@
 import Layout from "../components/layout";
 import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config";
+import ProjectItem from "../components/projects/project-item";
 
 export default function Projects({ projects }) {
   console.log(projects);
 
   return (
     <Layout>
-      <Head>
-        <title>빡코딩 포트폴리오</title>
-        <meta name="description" content="오늘도 빡코딩!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>총 프로젝트 : {projects.results.length}</h1>
-      {projects.results.map((aProject) => (
-        <h1>{aProject.properties.이름.title[0]?.plain_text}</h1>
-      ))}
+      <div className="flex flex-col items-center justify-center min-h-screen px-5 py-24 mb-10 px-6">
+        <Head>
+          <title>빡코딩 포트폴리오</title>
+          <meta name="description" content="오늘도 빡코딩!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트 :
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 mr-12 gap-8 w-full">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
